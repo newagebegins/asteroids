e323123df85af22c2ca7e9b0e1748af92a30f327
newagebegins <newagebegins@gmail.com>
2015-05-02 20:30:56 +1000

    Removed MyPolygon

--- win32/mygame_polygon.h
+++ win32/mygame_polygon.h
@@ -3,11 +3,6 @@
 
 #include "mygame_math.h"
 
-struct MyPolygon {
-	vec2 vertices[64];
-	int vertexCount;
-};
-
 MyRectangle getPolygonBounds(vec2 *polygon, int vertexCount);
 bool polygonsIntersect(vec2 *verticesA, int vertexCountA, vec2 *verticesB, int vertexCountB);
 bool isPointInPolygon(vec2 p, vec2 *vertices, int vertexCount);
